7d1048ad1e7e8783078fccef6a548ff7408771c5
Eidan Khan <eidan.khan659@gmail.com>
2025-03-22 12:37:12 +0000

    Using RestTemplate to call an external microservice API

--- catalog-service/src/main/java/io/javabrains/catalog/MovieCatalogResource.java
+++ catalog-service/src/main/java/io/javabrains/catalog/MovieCatalogResource.java
@@ -1,17 +1,32 @@
 package io.javabrains.catalog;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
     @GetMapping("/{userId}")
     public List<CatalogItem> getItems(@PathVariable("userId") String userId){
-        return Collections.singletonList(
-                new CatalogItem("Veer Zara", "Fantastic Love Story",4.9)
+
+        // Get Ratings (hardcoding ratings data for now)
+        List<Rating> ratings = Arrays.asList(
+                new Rating(123L, 4.5),
+                new Rating(456L, 3.8),
+                new Rating(789L, 4.2)
         );
+
+        // Get Movies using the RestTemplate
+        RestTemplate restTemplate = new RestTemplate();
+
+        return ratings.stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(), "Description", rating.getRatingValue());
+        }).collect(Collectors.toList());
     }
 }
---
+++ catalog-service/src/main/java/io/javabrains/catalog/Rating.java
@@ -0,0 +1,27 @@
+package io.javabrains.catalog;
+
+public class Rating {
+    private Long movieId;
+    private Double ratingValue;
+
+    public Rating(Long movieId, Double ratingValue) {
+        this.movieId = movieId;
+        this.ratingValue = ratingValue;
+    }
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+
+    public Double getRatingValue() {
+        return ratingValue;
+    }
+
+    public void setRatingValue(Double ratingValue) {
+        this.ratingValue = ratingValue;
+    }
+}
