9ecf8b158760fffd5a15b4fabac12e56f6c6ae2f
Eidan Khan <eidan.khan659@gmail.com>
2025-03-22 22:06:53 +0000

    Making a call to the Ratings API

--- rating-service/src/main/java/io/javabrains/rating/RatingResource.java
+++ rating-service/src/main/java/io/javabrains/rating/RatingResource.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/ratings")
 public class RatingResource {
@@ -12,5 +15,15 @@ public class RatingResource {
     public Rating getRating(@PathVariable Long movieId) {
         // Simulated rating retrieval from a database
         return new Rating(movieId, 4.29);
+    }
+
+    @GetMapping("/users/{userId}")
+    public List<Rating> getUserRatings(@PathVariable Long userId) {
+        // Simulated rating retrieval from a database
+        return Arrays.asList(
+                new Rating(123L, 4.5),
+                new Rating(456L, 3.8),
+                new Rating(789L, 4.2)
+        );
     }
 }
