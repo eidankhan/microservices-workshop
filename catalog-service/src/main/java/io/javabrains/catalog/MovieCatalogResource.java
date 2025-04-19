77bc7e17bd91da5a6e676426921a3b7bc7ad1943
Eidan Khan <eidan.khan659@gmail.com>
2025-04-19 15:11:20 +0100

    Discovering services through Eureka

--- catalog-service/src/main/java/io/javabrains/catalog/MovieCatalogResource.java
+++ catalog-service/src/main/java/io/javabrains/catalog/MovieCatalogResource.java
@@ -20,22 +20,25 @@ public class MovieCatalogResource {
     @GetMapping("/{userId}")
     public List<CatalogItem> getItems(@PathVariable("userId") String userId){
         // Get Ratings (hardcoding ratings data for now)
-        List<Rating> ratings = Arrays.asList(
-                new Rating(123L, 4.5),
-                new Rating(456L, 3.8),
-                new Rating(789L, 4.2)
-        );
+//        List<Rating> ratings = Arrays.asList(
+//                new Rating(123L, 4.5),
+//                new Rating(456L, 3.8),
+//                new Rating(789L, 4.2)
+//        );
+
+        UserRating userRatings = restTemplate.getForObject("http://rating-info-service/ratings/users/" + userId, UserRating.class);
 
         // Get Movies using the RestTemplate
-        return ratings.stream().map(rating -> {
-            // Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+        return userRatings.getRatings().stream().map(rating -> {
+            // Calling Eureka instances instead of hardcoded URLs
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
 
-            Movie movie = webClientBuilder.build()
-                    .get()
-                    .uri("http://localhost:8082/movies/"+rating.getMovieId())
-                    .retrieve()
-                    .bodyToMono(Movie.class)
-                    .block();
+//            Movie movie = webClientBuilder.build()
+//                    .get()
+//                    .uri("http://localhost:8082/movies/"+rating.getMovieId())
+//                    .retrieve()
+//                    .bodyToMono(Movie.class)
+//                    .block();
 
 
             return new CatalogItem(movie.getName(), "Description", rating.getRatingValue());
